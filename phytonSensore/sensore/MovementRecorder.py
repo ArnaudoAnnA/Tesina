775dabed163ad94f06784cda7f24b080e5bf3b4a
ArnaudoAnnA <38939245+ArnaudoAnnA@users.noreply.github.com>
2019-02-04 13:10:22 +0100

    Update MovementRecorder.py

--- phytonSensore/sensore/MovementRecorder.py
+++ phytonSensore/sensore/MovementRecorder.py
@@ -73,19 +73,21 @@ LenFifo = 10	#LAUGERO cosa sono queste variabili? (lunghezza di una coda first i
 Athr = 0.04
 AxFifo = collections.deque(LenFifo*[0], LenFifo) 	#crea una nuova coda utilizzando i dati passati
 AyFifo = collections.deque(LenFifo*[0], LenFifo)	# LenFifo*[0] crea un vettore di zeri. Il numero di zeri è contenuto nells variabile LenFifo
-AzFifo = collections.deque(LenFifo*[0], LenFifo)
+AzFifo = collections.deque(LenFifo*[0], LenFifo)	#A per accellerometro, G per giroscopio
 GxFifo = collections.deque(LenFifo*[0], LenFifo)
 GyFifo = collections.deque(LenFifo*[0], LenFifo)
 GzFifo = collections.deque(LenFifo*[0], LenFifo)
 
-csvfile = open('movements.csv', 'ab')
-writer = csv.writer(csvfile)
+csvfile = open('movements.csv', 'ab')			#creo un puntatore ad un file e lo apro in append mode e in binary mode
+writer = csv.writer(csvfile)				#creo una nuova istanza dell'oggetto writer (appartenente al modulo csv)
 
-movement_class = sys.argv[1] 	
+movement_class = sys.argv[1] 				#LAUGERO cos'è una classe di movimenti? 
 if not(movement_class):
 	movement_class = 'UNK'
 
-while True:
+while True:						#ciclo che all'infinito legge i dati di accellerometro e giroscopio e se è cambiato qualcosa
+							#rispetto al ciclo precedente, scrive i dati su file:
+							#in questo modo il file conterrà i diversi valori dei sensori a diverse quantità di tempo.
 	#Read Accelerometer raw value
 	acc_x = read_raw_data(ACCEL_XOUT_H)
 	acc_y = read_raw_data(ACCEL_YOUT_H)
@@ -96,7 +98,7 @@ while True:
 	gyro_y = read_raw_data(GYRO_YOUT_H)
 	gyro_z = read_raw_data(GYRO_ZOUT_H)
 	
-	#Full scale range +/- 250 degree/C as per sensitivity scale factor
+	#Full scale range +/- 250 degree/C as per sensitivity scale factor		#LAUGERO spiega
 	Ax = acc_x/16384.0
 	Ay = acc_y/16384.0
 	Az = acc_z/16384.0
@@ -105,7 +107,7 @@ while True:
 	Gy = gyro_y/131.0
 	Gz = gyro_z/131.0
 
-	AxFifo.append(Ax)
+	AxFifo.append(Ax)				#scrivo i dati registrati nei vettori (code) relativi
 	AyFifo.append(Ay)
 	AzFifo.append(Az)
 	GxFifo.append(Gx)
@@ -113,8 +115,15 @@ while True:
 	GzFifo.append(Gz)
 
 	movement_detected = (np.abs(AxFifo[LenFifo-1]-AxFifo[LenFifo-2])>Athr) | (np.abs(AyFifo[LenFifo-1]-AyFifo[LenFifo-2])>Athr) | (np.abs(AzFifo[LenFifo-1]-AzFifo[LenFifo-2])>Athr)
-
-	if movement_detected:
+							#np.abs() ricava il valore assoluto
+							#ricavo quindi la differenza tra gli ultimi due elementi dell'array e guardo se è maggiore di Athr (LAUGERO cos'è questa variabile?)
+							#in pratica guardo se è cambiato qualcosa nei valori registrati dall'accellerometro. Il cambiamento deve essere di un fattore maggiore di Athr.
+							#se è cambiato qualcosa, movement_detected vale true		
+							#LAUGERO ho scritto una cavolata?
+							
+				
+	if movement_detected:				#se è cambiato qualcosa scrivo su file 
+							#LAUGERO non mi è chiaro il modo in cui questi dati vengono scritti su file
 		#print '%.2f, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f' %(time.time(),Gx,Gy,Gz,Ax,Ay,Az)
 		writer.writerow([time.time()] + list(AxFifo) + list(AyFifo) + list(AzFifo) + list(GxFifo) + list(GyFifo) + list(GzFifo) + [movement_class])
 	#print 'Gx=%.2f, Gy=%.2f, Gz=%.2f, Ax=%.2f, Ay=%.2f, Az=%.2f' %(Gx,Gy,Gz,Ax,Ay,Az)	

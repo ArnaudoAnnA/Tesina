753791c4fb546f83a6b602f6dea4fa5bc56e1941
ArnaudoAnnA <38939245+ArnaudoAnnA@users.noreply.github.com>
2019-03-24 09:26:02 +0100

    Delete elenco_output_audio.py

--- Project/S3_Registrazione_esercizio/F1_SW_Pulsanti/elenco_output_audio.py
+++
@@ -1,5 +0,0 @@
-#ogni valore di costante verrà sostituito con il nome del file audio che contiene una registrazione che dice le cose scritte tra virgolette
-
-VALORE_SCARTATO = "valore scartato"
-SPIEGAZIONI_IMPOSTAZIONE_TIMER = "Per aumentare premi destra, per diminuire premi sinistra ecc...."
-CONFERMA_TIMER = "Vuoi confermare il numero selezionato? Destra per si, sinistra per no"

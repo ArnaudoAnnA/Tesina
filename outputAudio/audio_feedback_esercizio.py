84057660469943a234f6d4552cc2cca63d1902c2
ArnaudoAnnA <38939245+ArnaudoAnnA@users.noreply.github.com>
2019-04-12 10:47:25 +0200

    Add files via upload

---
+++ outputAudio/audio_feedback_esercizio.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+import time
+import audio_timer
+import raspberry_audio as outputInterface
+
+TEMPO_PRE_INIZIO_CORREZIONE = 5
+MINIMO_PERCENTUALE_CORRETTEZZA = 65
+
+class OutputCorrezioneEsercizio:
+  
+  correttezzaMedia = 0  #VOLENDO IN FUTURO SI PUO' AGGIUNGERE CORRETTEZZA MEDIA PER SENSORE
+  n_feedBack = 0
+
+  #funzione che si occupa del timer durante la fase di correzione esercizio
+  def outputTimer(tempo):
+    timer = audio_timer.Timer(tempo)
+
+    #conto alla rovescia prima dell'inizio 
+    timer_pre = Timer(TEMPO_PRE_INIZIO_CORREZIONE)
+    outputInterface.output_audio(FILE_AUDIO.INIZIO_REGISTRAZIONE_TRA_QUALCHE_SECONDO.format(SECONDI_PRE_REGISTRAZIONE))
+    timer_pre.audio_conto_alla_rovescia(1)
+
+    #inizio della fase di acquisizione e correzione
+    outputInterface.output_audio(FILE_AUDIO.VIA)                                                        #"via"
+    time.sleep(1)
+    timer.audio_conto_alla_rovescia(3)  #il numero di secondi rimanenti viene dato in output ogni tre secondi
+    
+    #alla fine, do in output il resoconto dell'esecuzione dell'esercizio
+    correttezzaMedia = correttezzaMedia / n_feedBack
+    outputInterface.outputAudio(FILE_AUDIO.RESOCONTO_ESERCIZIO.format(correttezzaMedia))
+
+
+  #funzione richiamata dai thread ogni volta che l'algoritmo di intelligenza artificiale riconosce un movimento
+  def feedback_movimento_notify(sensore, percentualeCorrettezza):
+
+    if(percentualeCorrettezza < MINIMO_PERCENTUALE_CORRETTEZZA):
+      outputInterface.(FILE_AUDIO.FEEDBACK_ERRORE.format(sensore))
+  
+    #salvo i dati per il resoconto 
+    correttezzaMedia += percentualeCorrettezza
+    n_feedBack += 1
---
+++ outputAudio/imposta_timer_per_registrazione.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+
+#------------------------------------------------------
+# versione Python 2
+
+#IMPOSTA_TIMER_PER_REGISTRAZIONE
+#si occupa della parte di impostazione del timer prima della registrazione dell'esercizio.
+
+#IMPORTANTE: richiede che sul raspberry sia installata la libreria Rpy.GPIO
+#------------------------------------------------------
+
+#costanti
+
+PIN_BOTTONE_DESTRA = 21
+PIN_BOTTONE_CENTRALE = 20
+PIN_BOTTONE_SINISTRA = 16
+
+
+
+TUTTO_SPENTO = 0
+TIMER_IN_IMPOSTAZIONE = 1
+TIMER_IMPOSTATO_REGISTRAZIONE_ESERCIZIO = 2
+
+SECONDI_PRE_REGISTRAZIONE = 5
+
+import FILE_AUDIO
+import RPi.GPIO as GPIO
+import time
+import audio_timer
+import raspberry_audio as outputInterface
+
+
+def click_bottone_sinistra(channel):
+    global statoTimer
+    if (statoTimer == TIMER_IN_IMPOSTAZIONE):
+        if (timerDaImpostare.timer != 0):
+            timerDaImpostare.incrementaTimer(-1)
+
+    elif (statoTimer == TIMER_IMPOSTATO_REGISTRAZIONE_ESERCIZIO):  # l'utente decide di scartare l'esercizio appena registrato
+        outputInterface.output_audio(FILE_AUDIO.VALORE_SCARTATO)
+        datasetEsercizio = [0]  #non è il caso di azzerare anche il timer perchè alla prossima pressione del tasto centrale verrà istanziato un nuovo oggetto timer
+        statoTimer = TUTTO_SPENTO
+
+# --------------------------------------------------------------------------------------------------------------------------------------------------
+
+def click_bottone_centrale(channel):
+    global statoTimer
+    global timerDaImpostare
+
+    if ( statoTimer == TUTTO_SPENTO):  # timer da impostare da capo (questo è il primo click sul tasto centrale)
+        outputInterface.output_audio("/home/pi/Downloads/Tesina-master/Project/S3_Registrazione_esercizio/F1_SW_Pulsanti/beep-01a.wav")
+        print("dovevo aver suonato")
+        statoTimer = TIMER_IN_IMPOSTAZIONE
+        timerDaImpostare = audio_timer.Timer(0)  #istazio un nuovo oggetto timer
+
+    elif (statoTimer == TIMER_IN_IMPOSTAZIONE and timerDaImpostare.timer != 0):  # l'utente ha terminato l'impostazione del timer
+        outputInterface.output_audio(FILE_AUDIO.CONFERMA_TIMER)
+        statoTimer = TIMER_IMPOSTATO_REGISTRAZIONE_ESERCIZIO
+
+
+# --------------------------------------------------------------------------------------------------------------------------------------------------
+
+def click_bottone_destra(channel):
+    global statoTimer
+    if (statoTimer == TIMER_IN_IMPOSTAZIONE):
+        timerDaImpostare.incrementaTimer(+1)
+
+    elif (statoTimer == TIMER_IMPOSTATO_REGISTRAZIONE_ESERCIZIO) : # l'utente decide di mantenere l'esercizio appena registrato
+        outputInterface.output_audio(FILE_AUDIO.INVIO_DATI_IN_CORSO)
+        audio_acquisizione_esercizio()
+        statoTimer = TUTTO_SPENTO
+
+# --------------------------------------------------------------------------------------------------------------------------------------------------
+# thread che, contemporaneamente a quello che memorizza l'esercizio, scandisce il tempo
+
+def audio_acquisizione_esercizio():
+
+    outputInterface.output_audio(FILE_AUDIO.INIZIO_REGISTRAZIONE_TRA_QUALCHE_SECONDO.format(SECONDI_PRE_REGISTRAZIONE))
+
+    conto_alla_rovescia_pre_registrazione = audio_timer.Timer(SECONDI_PRE_REGISTRAZIONE)
+    conto_alla_rovescia_pre_registrazione.audio_conto_alla_rovescia(1)                                   #"5..4..3..2.."
+
+    outputInterface.output_audio(FILE_AUDIO.VIA)                                                        #"via"
+    time.sleep(1)
+
+    timerDaImpostare.audio_conto_alla_rovescia(1)                                                        #"7..6..5..4.."
+
+    outputInterface.output_audio(FILE_AUDIO.REGISTRAZIONE_TERMINATA)                                    #"registrazione terminata"
+
+
+# ------- MAIN ----------------------------------------------------------------------------------------------------
+
+GPIO.setmode(GPIO.BCM)                              #specifico quale configurazione di pin intendo usare
+GPIO.setup(PIN_BOTTONE_SINISTRA, GPIO.IN, pull_up_down=GPIO.PUD_UP)  #PUD_DOWN significa che, se non viene ricevuto nessun segnale da raspberry, l'input del pin è di default 0
+                                                    #questa istruzione è importante per evitare errori dovuti a variazioni di tensione, che avvengono anche quando un pin non riceve voltaggio, per motivi fisici 
+GPIO.setup(PIN_BOTTONE_CENTRALE, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(PIN_BOTTONE_DESTRA, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+#la situazione inziale è "timer non impostato" e "esercizio non registrato"
+statoTimer = TUTTO_SPENTO;
+datasetEsercizio = [0]  #MOK
+timerDaImpostare = audio_timer.Timer(0)
+
+#aggiungo un event_detect ad ogni pin e associo la relativa funzione che gestirà l'evento click sul bottone
+#ulteriori spiegazioni:  https://sourceforge.net/p/raspberry-gpio-python/wiki/Inputs/
+#SINISTRA
+GPIO.add_event_detect(PIN_BOTTONE_SINISTRA, GPIO.FALLING)   #GPIO.FALLING significa che l'evento si scatena nel momento in cui il bottone viene premuto (non quando viene rilasciato)
+
+GPIO.add_event_callback(PIN_BOTTONE_SINISTRA, click_bottone_sinistra)
+
+
+#CENTRO
+GPIO.add_event_detect(PIN_BOTTONE_CENTRALE, GPIO.FALLING)
+GPIO.add_event_callback(PIN_BOTTONE_CENTRALE, click_bottone_centrale)
+
+#DESTRA
+GPIO.add_event_detect(PIN_BOTTONE_DESTRA, GPIO.FALLING)
+GPIO.add_event_callback(PIN_BOTTONE_DESTRA, click_bottone_destra)
+
+
+

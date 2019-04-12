fe49b665187624fc7a9d93209ada6c3522a65c87
ArnaudoAnnA <38939245+ArnaudoAnnA@users.noreply.github.com>
2019-04-12 10:51:35 +0200

    Add files via upload

--- Project/S1_Rilevamento_movimento/F6_SW_audio/raspberry_audio.py
+++ Project/S1_Rilevamento_movimento/F6_SW_audio/raspberry_audio.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 
 #------------------------------------------------------
@@ -6,18 +7,17 @@
 
 
 #RASPBERRY_AUDIO
-#questa libreria contiene funzioni per la riproduzione di file audio su raspberry Pi
+#questo modulo contiene funzioni per la riproduzione di file audio su raspberry Pi
 
 #------------------------------------------------------
-import os
+import soundplayer
+
+ID_AUDIOOUTPUT_DEVICE = 1
 
 
 # gestire interfaccia audio di Raspberry: https://www.raspberrypi.org/documentation/usage/audio/README.mdaa
 # utilizzo del modulo os: https://docs.python.org/2/library/os.html
+# utilizzo del modulo subprocess : https://docs.python.org/2/library/subprocess.html
 def output_audio(messaggio):
-    pid = os.forkpty()  # se non funziona usare pty.spawn()
-    if (pid == 0):  # sono nel figlio
-        {
-            # SE DA QUALCHE PROBLEMA POTREBBE ESSERE PERCHE' NON HO FATTO FLUSH DEI PRECENTI INPUT (si risolve con la funzione os.fsync)
-            os.exceclp("omxplayer", messaggio)  # eseguo il programma per la riproduzione dell'audio
-        }
+   p = soundplayer.SoundPlayer(messaggio, ID_AUDIOOUTPUT_DEVICE)
+   p.play()

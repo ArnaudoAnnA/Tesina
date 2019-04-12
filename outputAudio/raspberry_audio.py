0d0400a4bb751d003f7aea067e045186a336e589
ArnaudoAnnA <38939245+ArnaudoAnnA@users.noreply.github.com>
2019-04-12 12:11:31 +0200

    Update raspberry_audio.py

--- outputAudio/raspberry_audio.py
+++ outputAudio/raspberry_audio.py
@@ -10,14 +10,11 @@
 #questo modulo contiene funzioni per la riproduzione di file audio su raspberry Pi
 
 #------------------------------------------------------
-import soundplayer
-
-ID_AUDIOOUTPUT_DEVICE = 1
-
+import os
 
 # gestire interfaccia audio di Raspberry: https://www.raspberrypi.org/documentation/usage/audio/README.mdaa
 # utilizzo del modulo os: https://docs.python.org/2/library/os.html
 # utilizzo del modulo subprocess : https://docs.python.org/2/library/subprocess.html
 def output_audio(messaggio):
-   p = soundplayer.SoundPlayer(messaggio, ID_AUDIOOUTPUT_DEVICE)
-   p.play()
+   cmd = "omxplayer "+messaggio
+   os.system(cmd)

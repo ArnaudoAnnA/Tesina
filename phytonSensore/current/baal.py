1c5c3a9cddc2c1e82e1c2047dde1d12de5c8e2df
blancosss <37739768+blancosss@users.noreply.github.com>
2019-04-15 11:41:46 +0200

    laugero muori

--- phytonSensore/current/baal.py
+++ phytonSensore/current/baal.py
@@ -9,7 +9,6 @@ import config
 import sqlite3
 from sqlite3 import Error
 
-
 #some MPU6050 Registers and their Address
 PWR_MGMT_1   = 0x6B
 SMPLRT_DIV   = 0x19
@@ -23,6 +22,9 @@ GYRO_XOUT_H  = 0x43
 GYRO_YOUT_H  = 0x45
 GYRO_ZOUT_H  = 0x47
 bus = smbus.SMBus(1)
+
+ACCEL_PARSE_NUMBER = 16384.0
+GYRO_PARSE_NUMBER = 131.0
 
 def MPU_Init(Device_Address):
     #write to sample rate register
@@ -54,19 +56,17 @@ def read_raw_data(addr, Device_Address):
         value = value - 65536
     return value
 
-    #Creates 6 FIFO, one for each data recived from the MPU6050
-def init_Fifo(lenFifo, sensorFifo, Athr): #sensorFifo 0=ax 1=ay 2=az 3=mx 4=my 5=mz
+#Creates 6 FIFO, one for each data recived from the MPU6050
+def init_Fifo(lenFifo, sensorFifo): #sensorFifo 0=ax 1=ay 2=az 3=mx 4=my 5=mz
     sensorFifo[0] = collections.deque(lenFifo*[0], lenFifo)
     sensorFifo[1] = collections.deque(lenFifo*[0], lenFifo)
     sensorFifo[2] = collections.deque(lenFifo*[0], lenFifo)
     sensorFifo[3] = collections.deque(lenFifo*[0], lenFifo)
     sensorFifo[4] = collections.deque(lenFifo*[0], lenFifo)
     sensorFifo[5] = collections.deque(lenFifo*[0], lenFifo)
-    print(list(sensorFifo[5]))
     return sensorFifo
 
-
-    #Read Accelerometer raw value
+#Read Accelerometer raw value
 def read_sensor_data(sensor, Device_Address): #sensor 0=ax 1=ay 2=az 3=mx 4=my 5=mz
     sensor[0] = read_raw_data(ACCEL_XOUT_H, Device_Address)
     sensor[1] = read_raw_data(ACCEL_YOUT_H, Device_Address)
@@ -80,27 +80,22 @@ def read_sensor_data(sensor, Device_Address): #sensor 0=ax 1=ay 2=az 3=mx 4=my 5
 
 def parse_sensor_data(sensor): #sensor 0=ax 1=ay 2=az 3=mx 4=my 5=mz
     #Full scale range +/- 250 degree/C as per sensitivity scale factor
-    sensor[0] = sensor[0]/16384.0
-    sensor[1] = sensor[1]/16384.0
-    sensor[2] = sensor[2]/16384.0
-    sensor[3] = sensor[3]/131.0
-    sensor[4] = sensor[4]/131.0
-    sensor[5] = sensor[5]/131.0
+    sensor[0] = sensor[0] / ACCEL_PARSE_NUMBER
+    sensor[1] = sensor[1] / ACCEL_PARSE_NUMBER
+    sensor[2] = sensor[2] / ACCEL_PARSE_NUMBER
+    sensor[3] = sensor[3] / GYRO_PARSE_NUMBER
+    sensor[4] = sensor[4] / GYRO_PARSE_NUMBER
+    sensor[5] = sensor[5] / GYRO_PARSE_NUMBER
     return sensor
 
 #adds an element on each one of the 6 FIFO, automatically deleting the last
 def append_sensor_data(sensorFifo, sensor):
     sensorFifo[0].append(sensor[0])
-    print sensor
-    print sensorFifo
     sensorFifo[1].append(sensor[1])
     sensorFifo[2].append(sensor[2])
     sensorFifo[3].append(sensor[3])
     sensorFifo[4].append(sensor[4])
     sensorFifo[5].append(sensor[5])
-    print "after",sensor
-    print "after",sensorFifo
-
 
 #laugero commenta, infame
 def db_connect(db_file):

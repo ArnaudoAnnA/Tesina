5c75e6653497f976409793246bc71132b929da7a
blancosss <37739768+blancosss@users.noreply.github.com>
2019-04-15 10:47:27 +0200

    Update raspberry_audio.py

--- outputAudio/raspberry_audio.py
+++ outputAudio/raspberry_audio.py
@@ -15,7 +15,7 @@ import pygame
 # gestire interfaccia audio di Raspberry: https://www.raspberrypi.org/documentation/usage/audio/README.mdaa
 # utilizzo del modulo os: https://docs.python.org/2/library/os.html
 # utilizzo del modulo subprocess : https://docs.python.org/2/library/subprocess.html
-def output_audio(file_audio):
+def output_audio(path, file_audio):
     pygame.init()
     audio = pygame.mixer.Sound(file_audio)
     audio.play()

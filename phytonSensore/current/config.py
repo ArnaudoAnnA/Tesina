88cc73cabfc672b4fd0f3f0796db9a0434bf1810
ArnaudoAnnA <38939245+ArnaudoAnnA@users.noreply.github.com>
2019-04-29 10:17:15 +0200

    Update config.py

--- phytonSensore/current/config.py
+++ phytonSensore/current/config.py
@@ -7,18 +7,25 @@ import smbus
 #Recorder constants
 
 #FIFO LENGTH
-LENFIFO         = 10
+WINDOW_LENGHT            = 10
 #DATA ACQUISITION RATE (n per second)
-ACQUIRATE       = 25
+MEASUREMENT_EACH_SECOND  = 25
 #NUMBER OF ELEMENTS OVERLAPPED EVERY FIFO
-OVERLAP         = 4
+WINDOW_OVERLAP           = 4
 #number of datas sent from each sensor
-NDATA           = 6
+NDATA_EACH_SENSOR        = 6
 
 #defaul address for i2c is 0x68 but if we connect the AD0 pin to VCC it changes to 0x69
 Device_Address1 = 0x68  # MPU6050 ARM device address
 Device_Address2 = 0x69  # MPU6050 LEG device address
 
-#files for sensor datas
-LegFile = "leg.csv"
-ArmFile = "arm.csv"
+#PIN associati ai bottoni della mascherina
+PIN_BOTTONE_DESTRA = 21
+PIN_BOTTONE_CENTRALE = 20
+PIN_BOTTONE_SINISTRA = 16
+
+#posizioni in cui si può trovare un sensore
+SENSORPOSITION_LEGSX = "legsx"
+SENSORPOSITION_LEGDX = "legdx"
+SENSORPOSITION_ARMSX = "armsx"
+SENSORPOSITION_ARMDX = "armdx"

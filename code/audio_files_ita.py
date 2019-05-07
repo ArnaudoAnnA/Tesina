53c1a4f3f776c1848ef3f7752bd27e366e25cc5d
ArnaudoAnnA <38939245+ArnaudoAnnA@users.noreply.github.com>
2019-05-07 20:03:11 +0200

    Update audio_files_ita.py

--- code/audio_files_ita.py
+++ code/audio_files_ita.py
@@ -12,24 +12,32 @@ import config
 
 DIRECTORY_PATH = config.ROOT_PATH + "file/audio/"
 
-
+#setting timer
 DISCARDED_VALUE = "valore scartato"
 TIMER_SETTINGS_GUIDE = "istruzioni_impostazione_timer.wav"
 CONFIRM = "Vuoi confermare il numero selezionato? Destra per si, sinistra per no"
 SENDING_DATA = "invio dati in corso"
 REGISTRATION_WILL_START_IN = "la registrazione sarà avviata tra"
 
-GO = "via"
-REGISTRATION_ENDED = "registrazione terminata"
-
+#feedback 
 ERROR_FEEDBACK = "errore sensore" #sostituire, in seguito, al numero del sensore la sua posizione
 EXERCISE_DONE_WITH = "esercizio eseguito con"
-PERCENTAGE_OF_CORRECTNESS = "per cento di correttezza" 
-
-NUMBERS = ["zero.mp3", "uno.wav", "due.wav", "tre.wav", "quattro.wav", "cinque.wav"]
-SECONDS = "secondi"
-
+PERCENTAGE_OF_CORRECTNESS = "per cento di correttezza"
 
 #exercise selection
 NO_AVAIABLE_EXERCISE = "nessun esercizio disponibile"
 USE_THE_ARROWS_TO_SELECT_THE_EXERCISE = "usare le frecce per selezionare l'esercizio"
+
+
+#generals
+NUMBERS = ["zero.mp3", "uno.wav", "due.wav", "tre.wav", "quattro.wav", "cinque.wav"]
+SECONDS = "secondi"
+GO = "via"
+REGISTRATION_ENDED = "registrazione terminata"
+SENSOR_POSITION = 
+{
+  config.SENSORPOSITION_LEGSX = "legsx"
+  config.SENSORPOSITION_LEGDX = "legdx"
+  config.SENSORPOSITION_ARMSX = "armsx"
+  config.SENSORPOSITION_ARMDX = "armdx"
+}  

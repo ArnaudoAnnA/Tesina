bbc143f5f128d841a8a95f71ffd7883953ac6c83
ArnaudoAnnA <38939245+ArnaudoAnnA@users.noreply.github.com>
2019-05-07 18:43:03 +0200

    Update audio_feedback_exercise.py

--- code/audio_feedback_exercise.py
+++ code/audio_feedback_exercise.py
@@ -4,47 +4,43 @@
 import time
 import audio_timer
 import audio_raspberry as output_interface
-import audio_file_ita as audio
+import audio_file_ita as file_audio
 
-REGISTRATION_COMMENCES_IN_SECONDS = audio.REGISTRATION_COMMENCES_IN_SECONDS
-EXERCISE_REPORT = audio.EXERCISE_REPORT
-ERROR_FEEDBACK = audio.ERROR_FEEDBACK
-GO = audio.GO
-
-CORRECTION_TIME_SPAN = 5
+TIME_BEFORE_START = 5
 MINIMUM_CORRECTNESS_PERCENTAGE = 65
 
 class AudioFeedbackExercise:
   
   correctness_sum = 0
   n_feedback = 0
+  
   #function that manages the timer during the correction phase
   @staticmethod
-  def outputTimer(time):
+  def outputTimer(seconds):
     AudioFeedbackExercise.medium_correctness = 0
     AudioFeedbackExercise.n_feedback = 0
-    timer = audio_timer.Timer(time)
 
     #initial countdown
-    countdown = audio_timer.Timer(CORRECTION_TIME_SPAN)
-    output_interface.audio_output(REGISTRATION_COMMENCES_IN_SECONDS.format(REGISTRATION_TIME_SPAN))
+    countdown = audio_timer.Timer(TIME_BEFORE_START)
+    output_interface.audio_output(file_audio.REGISTRAZIONE_AVVIATA_TRA.format(TIME_BEFORE_START))
     countdown.audio_countdown(1)
 
     #acquisition and correction phase
-    output_interface.audio_output(GO)
+    output_interface.audio_output(file_audio.GO)
     time.sleep(1)
+    timer = audio_timer.Timer(seconds)
     timer.audio_countdown(1)  
     
     #report exercise execution
     medium_correctness = AudioFeedbackExercise.medium_correctness / AudioFeedbackExercise.n_feedback
-    output_interface.audio_output(EXERCISE_REPORT.format(medium_correctness))
+    output_interface.audio_output(file_audio.PERCENTUALE_CORRETTEZZA.format(medium_correctness))
 
   #function called by thread every time the AI algorithm recognizes a movement class
   @staticmethod
   def movement_feedback_notify(sensor, correctness_percentage):
 
     if(correctnessPercentage < MINIMUM_CORRECTNESS_PERCENTAGE):
-      output_interface.audio_output(ERROR_FEEDBACK.format(sensor))
+      output_interface.audio_output(file_audio.ERRORE_SENSORE.format(sensor))
   
     #saving report
     AudioFeedbackExercise.correctness_sum += correctness_percentage

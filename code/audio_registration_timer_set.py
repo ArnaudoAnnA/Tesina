fd8c08f7bf833267570cf1c692a619ed81ae9d2b
ArnaudoAnnA <38939245+ArnaudoAnnA@users.noreply.github.com>
2019-05-10 18:09:20 +0200

    DA TESTARE

--- code/audio_registration_timer_set.py
+++ code/audio_registration_timer_set.py
@@ -60,20 +60,20 @@ def central_button_click(channel):
 def right_button_click(channel):
     global timer_state
     global unset_timer
+    global callback_when_timer_setted
     
     if (timer_state == SETTING_TIMER):
         print(unset_timer.timer)
         unset_timer.increase_timer(+1)
 
     elif (timer_state == TIMER_SET) : # l'utente conferma il tempo impostato e procede con la registrazione dell'esercizio
-        exercise_acquisition_audio()
-        #volendo qui si può aggiungere una richiesta al'utente se intende salvare l'esercizio appena registrato
+        callback_when_timer_setted(unset_timer)
         timer_state = ALL_OFF
 
 # --------------------------------------------------------------------------------------------------------------------------------------------------
 # thread che, contemporaneamente a quello che memorizza l'esercizio, scandisce il tempo
 
-def exercise_acquisition_audio():
+def exercise_acquisition_audio(unset_timer):
 
     output_interface.output_audio(audio_files.DIRECTORY_PATH, [audio_files.REGISTRATION_WILL_START_IN, audio_files.NUMBERS[TIME_BEFORE_START], audio_files.SECONDI])
 
@@ -86,36 +86,43 @@ def exercise_acquisition_audio():
     unset_timer.audio_countdown(1)
 
     output_interface.audio_output(audio_files.DIRECTORY_PATH, [audio_files.REGISTRATION_ENDED])
+     #volendo qui si può aggiungere una richiesta al'utente se intende salvare l'esercizio appena registrato
 
 
-# ------- MAIN ----------------------------------------------------------------------------------------------------
-
-GPIO.setmode(GPIO.BCM)                              #specifico quale configurazione di pin intendo usare
-GPIO.setup(LEFT_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  #PUD_DOWN significa che, se non viene ricevuto nessun segnale da raspberry, l'input del pin è di default 0
-                                                    #questa istruzione è importante per evitare errori dovuti a variazioni di tensione, che avvengono anche quando un pin non riceve voltaggio, per motivi fisici 
-GPIO.setup(CENTRAL_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(RIGHT_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-#la situazione inziale è "timer non impostato" e "esercizio non registrato"
-timer_state = ALL_OFF;
-datasetEsercizio = [0]  #MOCK
+# ------- MAIN ---------------------------------------------------------------------------------------------------
+#globals
+timer_state = ALL_OFF
 unset_timer = audio_timer.Timer(0)
+callback = None
 
-#aggiungo un event_detect ad ogni pin e associo la relativa funzione che gestirà l'evento click sul bottone
-#ulteriori spiegazioni:  https://sourceforge.net/p/raspberry-gpio-python/wiki/Inputs/
-#SINISTRA
-GPIO.add_event_detect(LEFT_BUTTON_PIN, GPIO.FALLING)   #GPIO.FALLING significa che l'evento si scatena nel momento in cui il bottone viene premuto (non quando viene rilasciato)
+#NOTE: when the timer is setted, a callback function is called
+def set_timer(callback_when_timer_setted):
+    GPIO.setmode(GPIO.BCM)                              #specifico quale configurazione di pin intendo usare
+    GPIO.setup(LEFT_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  #PUD_DOWN significa che, se non viene ricevuto nessun segnale da raspberry, l'input del pin è di default 0
+                                                        #questa istruzione è importante per evitare errori dovuti a variazioni di tensione, che avvengono anche quando un pin non riceve voltaggio, per motivi fisici 
+    GPIO.setup(CENTRAL_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(RIGHT_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-GPIO.add_event_callback(LEFT_BUTTON_PIN, left_button_click)
+    #la situazione inziale è "timer non impostato" e "esercizio non registrato"
+    timer_state = ALL_OFF;
+    unset_timer = audio_timer.Timer(0)
+    callback = callback_when_timer_setted
+
+    #aggiungo un event_detect ad ogni pin e associo la relativa funzione che gestirà l'evento click sul bottone
+    #ulteriori spiegazioni:  https://sourceforge.net/p/raspberry-gpio-python/wiki/Inputs/
+    #SINISTRA
+    GPIO.add_event_detect(LEFT_BUTTON_PIN, GPIO.FALLING)   #GPIO.FALLING significa che l'evento si scatena nel momento in cui il bottone viene premuto (non quando viene rilasciato)
+
+    GPIO.add_event_callback(LEFT_BUTTON_PIN, left_button_click)
 
 
-#CENTRO
-GPIO.add_event_detect(CENTRAL_BUTTON_PIN, GPIO.FALLING)
-GPIO.add_event_callback(CENTRAL_BUTTON_PIN, central_button_click)
+    #CENTRO
+    GPIO.add_event_detect(CENTRAL_BUTTON_PIN, GPIO.FALLING)
+    GPIO.add_event_callback(CENTRAL_BUTTON_PIN, central_button_click)
 
-#DESTRA
-GPIO.add_event_detect(RIGHT_BUTTON_PIN, GPIO.FALLING)
-GPIO.add_event_callback(RIGHT_BUTTON_PIN, right_button_click)
+    #DESTRA
+    GPIO.add_event_detect(RIGHT_BUTTON_PIN, GPIO.FALLING)
+    GPIO.add_event_callback(RIGHT_BUTTON_PIN, right_button_click)
 
-while(True):
-    pass
+    while(True):
+        pass

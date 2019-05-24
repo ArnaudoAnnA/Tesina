a81523b240b65ce027a60bf0094a212af13a90a4
ArnaudoAnnA <38939245+ArnaudoAnnA@users.noreply.github.com>
2019-05-24 09:34:14 +0200

    Update audio_cuntdown.py

--- code/audio_cuntdown.py
+++ code/audio_cuntdown.py
@@ -3,8 +3,8 @@
 import vocal_synthesizer as vs
 import time
 
-def countdown(value):
-  for x in reversed(xrange(1, value):
+def start(initial_value):
+  for x in reversed(xrange(1, initial_value):
 		oldnow = time.time
 		vs.say(str(x))
 		time.sleep(1.0 - (time.time() - oldnow))

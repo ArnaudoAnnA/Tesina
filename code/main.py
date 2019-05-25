3f4c32d101d75a593f7325a18380ed6d3c203b17
ArnaudoAnnA <38939245+ArnaudoAnnA@users.noreply.github.com>
2019-05-25 17:57:02 +0200

    Update main.py

--- code/main.py
+++ code/main.py
@@ -3,24 +3,30 @@
 import main_functions as mf
 import db_functions ad db
 
-EXERCISE_ID = 0
-SECONDS = 0
+#REGISTRATION PHASE
+
 #init
 mf.init_sensor_vocal_synthesizer()
 
-#REGISTRATION PHASE
-EXERCISE_NAME = ""
-EXERCISE_DESCRIPTION = ""
-
-
+#esercizi e descrizioni vanno caricati su DB prima della registrazione
+"""
 db_exercises = db.Database()
 table_exercises = db_exercises.table_exercises
 if(!table_exercises.id_already_present(EXERCISE_ID)):
   table_exercises.add_new_exercise(EXERCISE_ID, EXERCISE_NAME, EXERCISE_DESCRIPTION)
- 
-mf.record_exercise(EXERCISE_ID, SECONDS)  
+"""
+While(True):
+    new_exercise_id = mf.select_new_exercise_id()
+    seconds = mf.select_seconds()
+    mf.record_exercise(new_exercise_id, seconds)  
 
 
 #CORRECTION PHASE
+#init
+mf.init_sensor_vocal_synthesizer()
 mf.init_AI()
-selected_exercise = mf.
+
+while(True):
+    exercise = mf.select_exercise()
+    seconds = mf.select_seconds()
+    mf.do_exercise(exercise, seconds)

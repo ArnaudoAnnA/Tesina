ea8191a553caf26335b8729ddf45ec8f36d97c51
ArnaudoAnnA <38939245+ArnaudoAnnA@users.noreply.github.com>
2019-05-27 15:15:36 +0200

    Update audio_cuntdown.py

--- code/audio_cuntdown.py
+++ code/audio_cuntdown.py
@@ -2,7 +2,7 @@
 
 
 
-import vocal_synthesizer as vs
+import vocal_synthesizer as output_interface
 import time
 
 
@@ -11,5 +11,5 @@ def start(initial_value):
 	"""cuntdown from initial_value to 0. Each second I give output remaining seconds"""
   	for x in reversed(xrange(1, initial_value):
 		oldnow = time.time
-		vs.say(str(x))
+		output_interface.output(str(x))
 		time.sleep(1.0 - (time.time() - oldnow))

81cc7751a7db9a01035392557e3f2c6ad755c9e0
ArnaudoAnnA <38939245+ArnaudoAnnA@users.noreply.github.com>
2019-05-27 22:25:43 +0200

    TESTED

--- code/audio_cuntdown.py
+++ code/audio_cuntdown.py
@@ -9,7 +9,7 @@ import time
 
 def start(initial_value):
 	"""cuntdown from initial_value to 0. Each second I give output remaining seconds"""
-  	for x in reversed(xrange(1, initial_value):
+  	for x in reversed(xrange(1, initial_value)):
 		oldnow = time.time
 		output_interface.output(str(x))
 		time.sleep(1.0 - (time.time() - oldnow))

fc7ec3fdf923a2c7b6e7928ecb523f49515914e3
ArnaudoAnnA <38939245+ArnaudoAnnA@users.noreply.github.com>
2019-06-08 10:17:55 +0200

    Update vocal_synthesizer.py

--- code/vocal_synthesizer.py
+++ code/vocal_synthesizer.py
@@ -3,6 +3,7 @@
 import config
 import pyttsx
 import threading
+import thread_semaphore as ts
 
 
 VS_RATE         = config.VS_RATE
@@ -11,9 +12,12 @@ VS_VOLUME       = config.VS_VOLUME
 
 
 thread = None
+semaphore = None
 
 def init():
         global thread
+        global semaphore
+        semaphore = ts.Semaphore()
         thread = Vocal_syntesizer()
         thread.start()
 
@@ -27,6 +31,8 @@ def output(string):
         thread.to_be_said = string      #text to speech start in a separate thread to avoid slowing 
         if(thread.engine.isBusy()):
                 thread.engine.stop()
+        while(semaphore.is_unlocked()): pass
+        semaphore.unlock()
 
 class Vocal_syntesizer(threading.Thread):
 
@@ -40,8 +46,10 @@ class Vocal_syntesizer(threading.Thread):
 
         def run(self):
                 """function that outputs the string given by audio through the vocal synthesizer"""
+                global semaphore
                 while(True):
-                        if(self.to_be_said != None):
-                                self.engine.say(self.to_be_said)
-                                self.to_be_said = None
-                                self.engine.runAndWait()
+                        semaphore.wait_for_unlock()
+                        self.engine.say(self.to_be_said)
+                        self.to_be_said = None
+                        self.engine.runAndWait()
+                        semaphore.lock()
